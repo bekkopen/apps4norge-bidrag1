cc42aa9a82b982848a71cdb6e301ecccf986a7f6
SindreNordbo <sindre.nordbo@bekk.no>
2013-02-21 17:30:55 +0100

    more parsing xml

--- src/no/bekk/Veibilde/VeibildeKartActivity.java
+++ src/no/bekk/Veibilde/VeibildeKartActivity.java
@@ -1,22 +1,22 @@
 package no.bekk.Veibilde;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-
+import no.bekk.Veibilde.domain.WeatherCamera;
+import no.bekk.Veibilde.service.AsyncTaskDelegate;
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
-import no.bekk.Veibilde.domain.WeatherCamera;
-import no.bekk.Veibilde.service.AsyncTaskDelegate;
 
-public class VeibildeKartActivity extends Activity implements AsyncTaskDelegate<WeatherCamera
-        > {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class VeibildeKartActivity extends Activity implements AsyncTaskDelegate<WeatherCamera> {
 	private LocationListener locationListener;
 	private GoogleMap veiBildeMap;
 
@@ -24,57 +24,61 @@ public class VeibildeKartActivity extends Activity implements AsyncTaskDelegate<
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.kart);
-		
+
 		MapFragment fragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
 		veiBildeMap = fragment.getMap();
 		veiBildeMap.setMyLocationEnabled(true);
-		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		locationListener = new MyLocationListener();
 		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+		// GetWeatherCameraAsyncTask task = new GetWeatherCameraAsyncTask(this);
+		// task.execute();
 	}
 
-    @Override
-    public void publishItem(WeatherCamera object) {
-        //draw weatherCamera here
-    }
+	@Override
+	public void publishItem(final WeatherCamera object) {
+		Log.w("VeilbildeKartActivity", "Found weatherCamera " + object.getId());
+	}
 
-    @Override
-    public void didFailWithError(String errorMessage) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+	@Override
+	public void didFailWithError(final String errorMessage) {
+		// To change body of implemented methods use File | Settings | File
+		// Templates.
+	}
 
-    @Override
-    public void didFinishProsess(String message) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
+	@Override
+	public void didFinishProsess(final String message) {
+		// To change body of implemented methods use File | Settings | File
+		// Templates.
+	}
 
-    class MyLocationListener implements LocationListener {
+	class MyLocationListener implements LocationListener {
 
 		@Override
-		public void onLocationChanged(Location location) {
-		    veiBildeMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15.0f));
-
-		    Toast.makeText(getApplicationContext(), "Lokasjon funnet", Toast.LENGTH_SHORT).show();
-			
+		public void onLocationChanged(final Location location) {
+			veiBildeMap
+					.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15.0f));
+			Toast.makeText(getApplicationContext(), "Lokasjon funnet", Toast.LENGTH_SHORT).show();
 		}
 
 		@Override
-		public void onProviderDisabled(String provider) {
+		public void onProviderDisabled(final String provider) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
-		public void onProviderEnabled(String provider) {
+		public void onProviderEnabled(final String provider) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
+		public void onStatusChanged(final String provider, final int status, final Bundle extras) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
 }
--- src/no/bekk/Veibilde/domain/WeatherCamera.java
+++ src/no/bekk/Veibilde/domain/WeatherCamera.java
@@ -1,23 +1,36 @@
 package no.bekk.Veibilde.domain;
 
 /**
- * Created with IntelliJ IDEA.
- * User: thomas johan eggum
- * Date: 2/21/13
- * Time: 2:55 PM
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: thomas johan eggum Date: 2/21/13 Time: 2:55
+ * PM To change this template use File | Settings | File Templates.
  */
 public class WeatherCamera {
 
-    private String id;
-    private String latitude;
-    private String longitude;
+	private String id;
+	private double latitude;
+	private double longitude;
 
-    public String getId() {
-        return id;
-    }
+	public double getLatitude() {
+		return latitude;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setLatitude(final double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(final double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(final String id) {
+		this.id = id;
+	}
 }
