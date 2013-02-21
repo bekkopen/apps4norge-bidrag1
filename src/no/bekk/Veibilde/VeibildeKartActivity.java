f810c4a0a9fb80f59fc43e0a1c9a8f9c76874c28
SindreNordbo <sindre.nordbo@bekk.no>
2013-02-21 15:33:00 +0100

    Added activity and permissions for map

---
+++ src/no/bekk/Veibilde/VeibildeKartActivity.java
@@ -0,0 +1,13 @@
+package no.bekk.Veibilde;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VeibildeKartActivity extends Activity {
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.kart);
+	}
+}
