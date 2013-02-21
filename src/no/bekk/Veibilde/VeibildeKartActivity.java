c0e2e0c8f6ddfcdde2ba42a8814e12432a20449d
Christoffer Marcussen <christoffer.marcussen@bekk.no>
2013-02-21 16:34:51 +0100

    Zoom in on user's location

--- src/no/bekk/Veibilde/VeibildeKartActivity.java
+++ src/no/bekk/Veibilde/VeibildeKartActivity.java
@@ -1,12 +1,21 @@
 package no.bekk.Veibilde;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class VeibildeKartActivity extends Activity {
+	private LocationListener locationListener;
+	private GoogleMap veiBildeMap;
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
@@ -14,8 +23,40 @@ public class VeibildeKartActivity extends Activity {
 		setContentView(R.layout.kart);
 		
 		MapFragment fragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-		GoogleMap map = fragment.getMap();
-		map.setMyLocationEnabled(true);
+		veiBildeMap = fragment.getMap();
+		veiBildeMap.setMyLocationEnabled(true);
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		locationListener = new MyLocationListener();
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+	}
+	
+	class MyLocationListener implements LocationListener {
+
+		@Override
+		public void onLocationChanged(Location location) {
+		    veiBildeMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15.0f));
+
+		    Toast.makeText(getApplicationContext(), "Lokasjon funnet", Toast.LENGTH_SHORT).show();
+			
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			
+		}
 		
 	}
 }
