dc5e79a7bca7fc5b367c03e1dc29348f8803f3ba
Christoffer Marcussen <christoffer.marcussen@bekk.no>
2013-02-21 15:52:07 +0100

    Display map

--- src/no/bekk/Veibilde/VeibildeKartActivity.java
+++ src/no/bekk/Veibilde/VeibildeKartActivity.java
@@ -1,5 +1,8 @@
 package no.bekk.Veibilde;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,5 +12,10 @@ public class VeibildeKartActivity extends Activity {
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.kart);
+		
+		MapFragment fragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+		GoogleMap map = fragment.getMap();
+		map.setMyLocationEnabled(true);
+		
 	}
 }
